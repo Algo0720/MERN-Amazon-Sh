1ec270ba7994dc7eb1d27827cb572b9d257891ef
Algo0720 <fox.sherif2014@gmail.com>
2022-05-05 14:18:22 +0200

    1 stage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import HomeScreen from "./screens/homeScreen";
+import ProductScreen from "./screens/productScreen";
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amazon-Sh</a>
-      </header>
-      <main>
-        list product
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazon-Sh</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
